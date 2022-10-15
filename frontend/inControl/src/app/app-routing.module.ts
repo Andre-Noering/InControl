4286276195b9e1aac60406059613c411510d3464
Cristian Monster <cmonster@furb.br>
2022-10-14 21:50:38 -0300

    login

--- frontend/inControl/src/app/app-routing.module.ts
+++ frontend/inControl/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'login', component: LoginScreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
