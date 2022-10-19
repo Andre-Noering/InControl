1d2d55eeccff03da7e83793fc21eb397fd45b247
Júlia de Amaral Montibeler <jamontibeler@furb.br>
2022-10-19 18:36:28 -0300

    cadastro

---
+++ frontend/inControl/src/app/estoque/estoque.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItensComponent } from './itens/itens.component';
+
+
+
+@NgModule({
+  declarations: [
+    ItensComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EstoqueModule { }
