17c0e495f390d7cd7e1d4f1dee537278d82c7b91
Andre-Noering <104037771+Andre-Noering@users.noreply.github.com>
2022-10-27 22:26:21 -0300

    Delete TRABALHO_FINAL directory

--- TRABALHO_FINAL/Loja Simulator/src/main/java/com/entra21/LojaSimulator/model/dto/ItemQtdeEstoqueDTO.java
+++
@@ -1,9 +0,0 @@
-package com.entra21.LojaSimulator.model.dto;
-
-public class ItemQtdeEstoqueDTO {
-    private Integer qtdeEstoque;
-
-    public ItemQtdeEstoqueDTO(Integer qtde_estoque) {
-        this.qtdeEstoque = qtde_estoque;
-    }
-}
